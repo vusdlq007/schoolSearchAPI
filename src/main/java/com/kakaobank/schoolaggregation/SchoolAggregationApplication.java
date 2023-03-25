668b6ce2c50af79a1457705a3c97023dc4fe8fb2
김윤권 <vusdlq007@naver.com>
2023-03-25 14:35:33 +0900

    Init Project

---
+++ src/main/java/com/kakaobank/schoolaggregation/SchoolAggregationApplication.java
@@ -0,0 +1,13 @@
+package com.kakaobank.schoolaggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAggregationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolAggregationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kakaobank/schoolaggregation/SchoolAggregationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaobank.schoolaggregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAggregationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
